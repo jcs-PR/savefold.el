e47d340d14158384d15641d3919992146755eb72
Jacob Fong <jacobcfong@gmail.com>
2025-04-01 16:00:40 -0700

    Add support for new text property org folding

--- savefold-org.el
+++ savefold-org.el
@@ -23,6 +23,25 @@
 ;;; Commentary:
 
 ;; This package implements persistence for org-mode visibility.
+;;
+;; There are many types of folds in an org file that should be persisted:
+;;
+;; - section/outline folds
+;; - blocks
+;; - drawers
+;; - babel results
+;;
+;; Under the traditional overlay-based ("old-fashioned") folding system, all of
+;; these are folded as overlays. Section/outline overlays can be made to
+;; identical effect with either outline-mode commands (ex
+;; `outline-hide-subtree') or org commands (ex `org-hide-entry'); they identify
+;; themselves via 'invisible spec as 'outline overlays. So do drawer folds. Thus
+;; we defer to existing savefold-outline logic for recovering those. A separate,
+;; org-specific system is built for persisting blocks and babel results.
+;;
+;; In the new text property based folding regime (org 9.6+), sections, blocks,
+;; and drawers are folded with text properties, while babel results are still
+;; overlay-based.
 
 ;;; Code:
 
@@ -43,52 +62,119 @@ reason for this to be non-nil."
   :type 'boolean
   :group 'savefold)
 
-(defvar savefold-org--org-fold-specs '(org-hide-block org-babel-hide-result))
+(defun savefold-org--make-overlay-fold (fold-data)
+  "Make overlay-based fold from FOLD-DATA."
+  (let ((start (car fold-data))
+        (end (cadr fold-data))
+        (spec (caddr fold-data)))
+    (cond
+     ((eq spec 'org-babel-hide-result)
+      (save-excursion
+        (goto-char start)
+        (org-babel-hide-result-toggle-maybe)))
+     (t (org-flag-region start end t spec)))))
 
-(defvar savefold-org--folds-attr 'savefold-org-folds)
+;;; Old-fashioned folding
+
+(defvar savefold-org--old-fashioned-folds-attr 'savefold-org-old-fashioned-folds)
+
+(defvar savefold-org--old-fashioned-fold-specs '(org-hide-block org-babel-hide-result))
+
+(defun savefold-org--recover-old-fashioned-folds ()
+  ;; Get outline folds manually if necessary
+  (when (and (not savefold-org-inhibit-outline-integration)
+             (not savefold-outline-mode))
+    (savefold-outline--recover-folds))
+
+  ;; Overlay folds
+  (mapc
+   'savefold-org--make-overlay-fold
+   (savefold-utils--get-file-attr savefold-org--old-fashioned-folds-attr)))
+
+(defun savefold-org--using-old-fashioned-foldsp ()
+  "Check whether using org<9.6 overlay-based folds or new text prop folds."
+  (or (version< org-version "9.6") (eq org-fold-core-style 'overlays)))
+
+(defun savefold-org--old-fashioned-foldp (ov)
+  "Check whether OV is an overlay for an old fashioned org-mode fold.
+
+This does not include folds made with `org-flag-region' with an 'outline
+invisibility spec, but only the invisibility specs exclusive to org-mode:
+`savefold-org--old-fashioned-fold-specs'."
+  (memq (overlay-get ov 'invisible) savefold-org--old-fashioned-fold-specs))
+
+(defun savefold-org--save-old-fashioned-folds ()
+  ;; Save outline folds manually if necessary
+  (when (and (not savefold-org-inhibit-outline-integration)
+             (not savefold-outline-mode))
+    (savefold-outline--save-folds))
+
+  ;; Save overlays
+  (savefold-utils--set-file-attr
+   savefold-org--old-fashioned-folds-attr
+   (mapcar
+    (lambda (ov)
+      `(,(overlay-start ov) ,(overlay-end ov) ,(overlay-get ov 'invisible)))
+    (savefold-utils--get-overlays 'savefold-org--old-fashioned-foldp)))
+
+  (savefold-utils--set-file-attr-modtime)
+  (savefold-utils--write-out-file-attrs))
+
+;;; Current folding
+
+(defvar savefold-org--text-prop-folds-attr 'savefold-org-text-prop-folds)
+
+(defvar savefold-org--overlay-folds-attr 'savefold-org-overlay-folds)
+
+(defvar savefold-org--text-prop-fold-specs
+  '(org-fold-outline org-fold-block org-fold-drawer))
+
+(defvar savefold-org--overlay-fold-specs '(org-babel-hide-result))
 
 (defun savefold-org--recover-folds ()
   "Read and apply saved org fold data for the current buffer."
   (savefold-utils--unless-file-recently-modified
-   (progn
-     (when (and (not savefold-org-inhibit-outline-integration)
-                (not savefold-outline-mode))
-       (savefold-outline--recover-folds))
+   (if (savefold-org--using-old-fashioned-foldsp)
+       (savefold-org--recover-old-fashioned-folds)
+     ;; Recover text prop folds
      (mapc
-      (lambda (fold-data)
-        (let ((start (car fold-data))
-              (end (cadr fold-data))
-              (spec (caddr fold-data)))
-          (cond
-           ((eq spec 'org-babel-hide-result)
-            (save-excursion
-              (goto-char start)
-              (org-babel-hide-result-toggle-maybe)))
-           (t (org-flag-region start end t spec)))))
-      (savefold-utils--get-file-attr savefold-org--folds-attr)))))
+      (lambda (fold)
+        (let ((start (car fold))
+              (end (cadr fold))
+              (spec (caddr fold)))
+          (org-fold-region start end t spec)))
+      (savefold-utils--get-file-attr savefold-org--text-prop-folds-attr))
 
-(defun savefold-org--org-foldp (ov)
-  "Check whether the overlay is for an org-mode fold.
+     ;; Recover overlay folds
+     (mapc
+      'savefold-org--make-overlay-fold
+      (savefold-utils--get-file-attr savefold-org--overlay-folds-attr)))))
 
-This does not include folds made with `org-flag-region' with an 'outline
-invisibility spec, but only the invisibility specs exclusive to org-mode:
-`savefold-org--org-fold-specs'."
-  (memq (overlay-get ov 'invisible) savefold-org--org-fold-specs))
+(defun savefold-org--overlay-foldp (ov)
+  "Check whether OV is an overlay for an org-mode fold."
+  (memq (overlay-get ov 'invisible) savefold-org--overlay-fold-specs))
 
 (defun savefold-org--save-folds ()
   "Save org fold data for the current buffer."
   (when (not (buffer-modified-p))
-    (when (and (not savefold-org-inhibit-outline-integration)
-               (not savefold-outline-mode))
-      (savefold-outline--save-folds))
-    (savefold-utils--set-file-attr
-     savefold-org--folds-attr
-     (mapcar
-      (lambda (ov)
-        `(,(overlay-start ov) ,(overlay-end ov) ,(overlay-get ov 'invisible)))
-      (savefold-utils--get-overlays 'savefold-org--org-foldp)))
-    (savefold-utils--set-file-attr-modtime)
-    (savefold-utils--write-out-file-attrs)))
+    (if (savefold-org--using-old-fashioned-foldsp)
+        (savefold-org--save-old-fashioned-folds)
+
+      ;; Save text property folds
+      (savefold-utils--set-file-attr
+       savefold-org--text-prop-folds-attr
+       (org-fold-get-regions :specs savefold-org--text-prop-fold-specs))
+
+      ;; Save overlay folds
+      (savefold-utils--set-file-attr
+       savefold-org--overlay-folds-attr
+       (mapcar
+        (lambda (ov)
+          `(,(overlay-start ov) ,(overlay-end ov) ,(overlay-get ov 'invisible)))
+        (savefold-utils--get-overlays 'savefold-org--overlay-foldp)))
+
+      (savefold-utils--set-file-attr-modtime)
+      (savefold-utils--write-out-file-attrs))))
 
 (defun savefold-org--set-up-save-on-kill-buffer ()
   (add-hook 'kill-buffer-hook 'savefold-org--save-folds nil t))
--- tests/test-savefold-org.el
+++ tests/test-savefold-org.el
@@ -26,93 +26,237 @@
 
 ;;; Code:
 
+;; Use text property style folding when available. Assume org is not loaded at
+;; this point.
+;; Probably should also test old-fashioned folding in the newer versions
+(when (version<= "29" emacs-version)
+    (setq org-fold-core-style 'text-properties))
+
 (require 'savefold-org)
 (require 'savefold-test-utils)
 
-(describe "savefold-org-mode"
-  :var* ((source-fpath
-          (expand-file-name "fixtures/org/lotr.org" savefold-test-utils--dir))
-         (attr-fpath
-          (expand-file-name "fixtures/org/attr-table" savefold-test-utils--dir)))
+(defun test-savefold-org--old-fashioned ()
+  (describe "savefold-org-mode (old-fashioned folds)"
+    :var* ((source-fpath
+            (expand-file-name
+             "fixtures/org/test.org" savefold-test-utils--dir))
+           (attr-fpath
+            (expand-file-name
+             "fixtures/org/old-fashioned-attr-table" savefold-test-utils--dir)))
 
-  (describe "when on"
-    (before-all
-      (savefold-org-mode 1))
+    (describe "when on"
+      (before-all
+        (savefold-org-mode 1))
 
-    (it "does not recover folds upon file open if file was recently modified"
-      (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
-        (savefold-utils--set-file-attr 'savefold-modtime '(0 0 0 0) temp-source-fpath)
-        (savefold-utils--write-out-file-attrs temp-source-fpath)
+      (it "does not recover folds upon file open if file was recently modified"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-utils--set-file-attr 'savefold-modtime '(0 0 0 0) temp-source-fpath)
+          (savefold-utils--write-out-file-attrs temp-source-fpath)
 
-        (savefold-test-utils--with-open-file temp-source-fpath
-          (expect
-           (not (savefold-utils--get-overlays 'savefold-org--org-foldp))))))
+          (savefold-test-utils--with-open-file temp-source-fpath
+            (expect
+             (and
+              (not
+               (savefold-utils--get-overlays 'savefold-outline--outline-foldp))
+              (not
+               (savefold-utils--get-overlays 'savefold-org--old-fashioned-foldp)))))))
 
-    (it "recovers folds upon file open"
-      (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
-        (savefold-test-utils--with-open-file temp-source-fpath
+      (it "recovers folds upon file open"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            ;; Outline folds
+            (expect
+             (savefold-test-utils--sets-equalp
+              (mapcar
+               (lambda (ov) `(,(overlay-start ov) ,(overlay-end ov)))
+               (savefold-utils--get-overlays 'savefold-outline--outline-foldp))
+              (savefold-utils--get-file-attr savefold-outline--folds-attr)))
+
+            ;; Strictly org folds
+            (expect
+             (savefold-test-utils--sets-equalp
+              (mapcar
+               (lambda (ov)
+                 `(,(overlay-start ov) ,(overlay-end ov) ,(overlay-get ov 'invisible)))
+               (savefold-utils--get-overlays 'savefold-org--old-fashioned-foldp))
+              (savefold-utils--get-file-attr savefold-org--old-fashioned-folds-attr))))))
+
+      (it "saves folds upon file close"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            (goto-char 773)
+            (org-hide-drawer-toggle)
+            (goto-char 896)
+            (org-hide-block-toggle)
+            (goto-char 964)
+            (org-babel-hide-result-toggle)
+            (goto-char 1057)
+            (org-hide-entry))
+
           ;; Outline folds
           (expect
            (savefold-test-utils--sets-equalp
-            (mapcar
-             (lambda (ov) `(,(overlay-start ov) ,(overlay-end ov)))
-             (savefold-utils--get-overlays 'savefold-outline--outline-foldp))
-            (savefold-utils--get-file-attr savefold-outline--folds-attr)))
+            (savefold-utils--get-file-attr savefold-outline--folds-attr temp-source-fpath)
+            '((786 819) (676 705) (367 400) (1087 1116))))
 
           ;; Strictly org folds
           (expect
            (savefold-test-utils--sets-equalp
-            (mapcar
-             (lambda (ov)
-               `(,(overlay-start ov) ,(overlay-end ov) ,(overlay-get ov 'invisible)))
-             (savefold-utils--get-overlays 'savefold-org--org-foldp))
-            (savefold-utils--get-file-attr savefold-org--folds-attr))))))
+            (savefold-utils--get-file-attr savefold-org--old-fashioned-folds-attr temp-source-fpath)
+            '((974 1003 org-babel-hide-result)
+              (913 962 org-hide-block)
+              (558 587 org-babel-hide-result)
+              (497 546 org-hide-block)))))))
 
-    (it "saves folds upon file close"
-      (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
-        (savefold-test-utils--with-open-file temp-source-fpath
-          (goto-char 548)
-          (org-cycle)
-          (goto-char 819)
-          (outline-show-subtree))
+    (describe "when turned back off"
+      (before-all
+        (savefold-org-mode -1))
 
-        ;; Outline folds
-        (expect
-         (savefold-test-utils--sets-equalp
-          (savefold-utils--get-file-attr savefold-outline--folds-attr temp-source-fpath)
-          '((111 414))))
+      (it "does not recover folds upon file open"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            (expect
+             (and
+              (not
+               (savefold-utils--get-overlays 'savefold-outline--outline-foldp))
+              (not
+               (savefold-utils--get-overlays 'savefold-org--old-fashioned-foldp)))))))
 
-        ;; Strictly org folds
-        (expect
-         (savefold-test-utils--sets-equalp
-          (savefold-utils--get-file-attr savefold-org--folds-attr temp-source-fpath)
-          '((757 817 org-babel-hide-result) (558 616 org-babel-hide-result)))))))
+      (it "does not save folds upon file close"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            (ignore))
 
-  (describe "when turned back off"
-    (before-all
-      (savefold-org-mode -1))
-
-    (it "does not recover folds upon file open"
-      (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
-        (savefold-test-utils--with-open-file temp-source-fpath
+          ;; Outline folds
           (expect
-           (not (savefold-utils--get-overlays 'savefold-org--org-foldp))))))
+           (savefold-test-utils--sets-equalp
+            (savefold-utils--get-file-attr savefold-outline--folds-attr temp-source-fpath)
+            '((676 705) (367 400))))
 
-    (it "does not save folds upon file close"
-      (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
-        (savefold-test-utils--with-open-file temp-source-fpath
-          (ignore))
+          ;; Strictly org folds
+          (expect
+           (savefold-test-utils--sets-equalp
+            (savefold-utils--get-file-attr savefold-org--old-fashioned-folds-attr temp-source-fpath)
+            '((558 587 org-babel-hide-result)
+              (497 546 org-hide-block)))))))))
 
-        ;; Outline folds
-        (expect
-         (savefold-test-utils--sets-equalp
-          (savefold-utils--get-file-attr savefold-outline--folds-attr temp-source-fpath)
-          '((111 414) (846 2959))))
+(defun test-savefold-org--current ()
+  (describe "savefold-org-mode (text-property folds)"
+    :var* ((source-fpath
+            (expand-file-name "fixtures/org/test.org" savefold-test-utils--dir))
+           (attr-fpath
+            (expand-file-name "fixtures/org/current-attr-table" savefold-test-utils--dir)))
 
-        ;; Strictly org folds
-        (expect
-         (savefold-test-utils--sets-equalp
-          (savefold-utils--get-file-attr savefold-org--folds-attr temp-source-fpath)
-          '((757 817 org-babel-hide-result))))))))
+    (describe "when on"
+      (before-all
+        (savefold-org-mode 1))
+
+      (it "does not recover folds upon file open if file was recently modified"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-utils--set-file-attr 'savefold-modtime '(0 0 0 0) temp-source-fpath)
+          (savefold-utils--write-out-file-attrs temp-source-fpath)
+
+          (savefold-test-utils--with-open-file temp-source-fpath
+            ;; Text property folds
+            (expect
+             (not
+              (org-fold-get-regions :specs savefold-org--text-prop-fold-specs)))
+
+            ;; Overlay folds
+            (expect
+             (not
+              (savefold-utils--get-overlays 'savefold-org--overlay-foldp))))))
+
+      (it "recovers folds upon file open"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            ;; Text property folds
+            (expect
+             (savefold-test-utils--sets-equalp
+              (org-fold-get-regions :specs savefold-org--text-prop-fold-specs)
+              (savefold-utils--get-file-attr savefold-org--text-prop-folds-attr)))
+
+            ;; Overlay folds
+            (expect
+             (savefold-test-utils--sets-equalp
+              (mapcar
+               (lambda (ov)
+                 `(,(overlay-start ov) ,(overlay-end ov) ,(overlay-get ov 'invisible)))
+               (savefold-utils--get-overlays 'savefold-org--overlay-foldp))
+              (savefold-utils--get-file-attr savefold-org--overlay-folds-attr))))))
+
+      (it "saves folds upon file close"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            (goto-char 773)
+            (org-fold-hide-drawer-toggle)
+            (goto-char 896)
+            (org-fold-hide-block-toggle)
+            (goto-char 964)
+            (org-babel-hide-result-toggle)
+            (goto-char 1057)
+            (org-fold-hide-entry))
+
+          ;; Text property folds
+          (expect
+           (savefold-test-utils--sets-equalp
+            (savefold-utils--get-file-attr
+             savefold-org--text-prop-folds-attr temp-source-fpath)
+            '((786 819 org-fold-drawer)
+              (367 400 org-fold-drawer)
+              (913 962 org-fold-block)
+              (497 546 org-fold-block)
+              (1087 1116 org-fold-outline)
+              (676 705 org-fold-outline))))
+
+          ;; Overlay folds
+          (expect
+           (savefold-test-utils--sets-equalp
+            (savefold-utils--get-file-attr
+             savefold-org--overlay-folds-attr temp-source-fpath)
+            '((558 587 org-babel-hide-result)
+              (974 1003 org-babel-hide-result)))))))
+
+    (describe "when turned back off"
+      (before-all
+        (savefold-org-mode -1))
+
+      (it "does not recover folds upon file open"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            ;; Text property folds
+            (expect
+             (not
+              (org-fold-get-regions :specs savefold-org--text-prop-fold-specs)))
+
+            ;; Overlay folds
+            (expect
+             (not
+              (savefold-utils--get-overlays 'savefold-org--overlay-foldp))))))
+
+      (it "does not save folds upon file close"
+        (savefold-test-utils--with-temp-savefold-environment source-fpath attr-fpath
+          (savefold-test-utils--with-open-file temp-source-fpath
+            (ignore))
+
+          ;; Text property folds
+          (expect
+           (savefold-test-utils--sets-equalp
+            (savefold-utils--get-file-attr
+             savefold-org--text-prop-folds-attr temp-source-fpath)
+            '((367 400 org-fold-drawer)
+              (497 546 org-fold-block)
+              (676 705 org-fold-outline))))
+
+          ;; Overlay folds
+          (expect
+           (savefold-test-utils--sets-equalp
+            (savefold-utils--get-file-attr
+             savefold-org--overlay-folds-attr temp-source-fpath)
+            '((558 587 org-babel-hide-result)))))))))
+
+(if (savefold-org--using-old-fashioned-foldsp)
+    (test-savefold-org--old-fashioned)
+  (test-savefold-org--current))
 
 ;;; test-savefold-org.el ends here
